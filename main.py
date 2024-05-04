78a5c1b29a8b1b50cd43c6bec3096a8dde9fe25c
siv <subhashpadala18@gmail.com>
2024-05-04 12:35:18 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import wandb
+import argparse
+from utils import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='google/vit-base-patch16-224', help='huggingface hub ckpt location')
+parser.add_argument('--dataset', type=str, default="imagenet-1k", help='huggingface hub dataset location')
+parser.add_argument('--split', type=str, default="validation", help="which dataset split to load")
+parser.add_arugment('--wandb', action='store_true')
+parser.add_argument('--save', action='store_true')
+args = parser.parse_args()
+    
+if __name__ == "__main__":
+    os.environ['WANDB_API_KEY'] = "0aa429d933365fa8e91048dd017a5410ef2a8c51"
+    os.environ['HF_TOKEN'] = "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"
+    
+    image_processor, model = prepare_model(args['model'])
+    dataset = prepare_dataset(args['dataset'], args['split'])
+    
+    logits_np, labels_np = evaluate(image_processor, model, dataset)
+    
+    calibrate_and_plot(logits_np, labels_np)
---
+++ utils.py
@@ -0,0 +1,78 @@
+import tqdm
+import torch
+import metrics
+import visualization
+import torch.nn.functional as F
+from datasets import load_dataset
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+
+
+def prepare_model(ckpt_name):
+    # load_dataset, load_model
+    image_processor = AutoImageProcessor.from_pretrained(ckpt_name)
+    model = AutoModelForImageClassification.from_pretrained(ckpt_name)
+    return image_processor, model
+
+def prepare_dataset(dataset_loc, split):
+    dataset = load_dataset(dataset_loc, split=split, streaming=True, trust_remote_code=True).with_format('torch')
+    newdataset = dataset.shuffle(seed=42, buffer_size=11_500).take(11500)
+    return newdataset
+
+def evaluate(image_processor, model, dataset):
+    # evaluate, generate_plots.
+    correct = 0
+    total = 0
+
+    logits_list = []
+    labels_list = []
+    broken_ids = []
+
+    with torch.no_grad():
+
+        for i, data in enumerate(tqdm(iter(dataset))):
+            try:
+                inputs = image_processor(data['image'], return_tensors="pt")
+                if inputs['pixel_values'].shape == 4:
+                    inputs['pixel_values'] = inputs['pixel_values'].squeeze(1)
+            except:
+                broken_ids.append(i)
+                continue
+            outputs = model(**inputs)
+            logits = outputs.logits
+            labels = data['label'].unsqueeze(dim=0)
+
+            logits_list.append(logits)
+            labels_list.append(labels)
+
+            output_probs = F.softmax(logits,dim=1)
+            probs, predicted = torch.max(output_probs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            if len(labels_list)==1000:
+                break
+        print('Broken images, which are not included: %d' % (len(broken_ids)))
+        print('Accuracy on the 10,000 validation images: %d %%' % (100 * correct / total))
+        print(f'number of images used {total}') 
+
+        logits_np = torch.cat(logits_list).numpy()
+        labels_np = torch.cat(labels_list).numpy()
+
+    return logits_np, labels_np
+
+def calibrate_and_plot(logits_np, labels_np):
+    ece_criterion = metrics.ECELoss()
+    sce_criterion = metrics.SCELoss()
+
+    print('ECE: %f' % (ece_criterion.loss(logits_np,labels_np, 15)))
+    print('SCE: %f' % (sce_criterion.loss(logits_np,labels_np, 15)))
+
+    conf_hist = visualization.ConfidenceHistogram()
+    plt_test = conf_hist.plot(logits_np,labels_np,title="Confidence Histogram")
+    plt_test.savefig('conf_histogram_test.png',bbox_inches='tight')
+    plt_test.show()
+
+    rel_diagram = visualization.ReliabilityDiagram()
+    plt_test_2 = rel_diagram.plot(logits_np,labels_np,title="Reliability Diagram")
+    plt_test_2.savefig('rel_diagram_test.png',bbox_inches='tight')
+    plt_test_2.show()
